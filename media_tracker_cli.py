e097f09701e9b800065da0ddca4c13f8ff06da2b
Aiden Schulman <aidenhschulman@gmail.com>
2023-03-01 23:25:32 -0500

    Created show method

--- media_tracker_cli.py
+++ media_tracker_cli.py
@@ -31,3 +31,36 @@ def complete(position: int):
     show()
 
 
+@app.command(short_help="Displays all of the items in the list")
+def show():
+    task = [("Attack on Titan", "Completed"), ("Gurren Lagann", "Completed")]
+    console.print("[bold magenta]Task[/bold magenta]", "💻")
+
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("#", style="dim", width=6)
+    table.add_column("Task", min_width=20)
+    table.add_column("Category", min_width=12, justify="right")
+    table.add_column("Done", min_width=12, justify="right")
+
+    def get_category_color(category):
+        COLORS = {
+            "Anime": "cyan",
+            "Manga": "cyan",
+            "TV": "red",
+            "Movie": "red",
+        }
+
+        if category in COLORS:
+            return COLORS[category]
+        return "white"
+
+    for index, task in enumerate(task, start=1):
+        c = get_category_color(task[1])
+        is_done_str = "✅" if True == 2 else "⏳"
+        table.add_row(str(index), task[0],
+                      f"[{c}]{task[1]}[/{c}]", is_done_str)
+    console.print(table)
+
+
+if __name__ == "__main__":
+    app()
