0e8a9471fc320565ca7810781515cb2c86fc6bde
Aiden Schulman <aidenhschulman@gmail.com>
2023-03-01 23:37:48 -0500

    Added create_table method

--- database.py
+++ database.py
@@ -2,3 +2,23 @@ import sqlite3
 from typing import List
 import datetime
 from model import Todo
+
+conn = sqlite3.connect("todos.db")
+c = conn.cursor()
+
+
+def create_table():
+    c.execute("""CREATE TABLE IF NOT EXISTS todos (
+        task text,
+        category text,
+        date_added text,
+        date_completed text,
+        status integer,
+        position integer
+        )""")
+
+
+create_table()
+
+
+
