cc8b2107721349bbbdb23124f55aa5b4e234c07d
Aiden Schulman <aidenhschulman@gmail.com>
2023-03-02 00:00:22 -0500

    Imported database methods, added to UI methods

--- media_tracker_cli.py
+++ media_tracker_cli.py
@@ -1,6 +1,8 @@
 import typer
 from rich.console import Console
 from rich.table import Table
+from model import Todo
+from database import get_all_todos, delete_todo, insert_todo, complete_todo, update_todo
 
 console = Console()
 
@@ -10,24 +12,30 @@ app = typer.Typer()
 @app.command(short_help="Adds an item to the list")
 def add(task: str, category: str):
     typer.echo(f"adding {task}, {category}")
+    todo = Todo(task, category)
+    insert_todo(todo)
     show()
 
 
 @app.command(short_help="Deletes an item from the list")
 def delete(position: int):
     typer.echo(f"deleting {position}")
+    # index in UI begins at 1, but in database begins at 0
+    delete_todo(position-1)
     show()
 
 
 @app.command(short_help="Update an item's properties")
 def update(position: int, task: str = None, category: str = None):
     typer.echo(f"updating {position}")
+    update_todo(position-1, task, category)
     show()
 
 
 @app.command(short_help="Marks an item as complete")
 def complete(position: int):
     typer.echo(f"complete {position}")
+    complete_todo(position-1)
     show()
 
 
